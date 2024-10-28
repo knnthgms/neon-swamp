c70642986725262f67a72dc33bb159a80dcb9ee5
kengomes <knnthgms@gmail.com>
2024-10-28 16:30:23 +0530

    Fixing broken links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
-import { Row, Col, Layout } from "antd";
+import { Layout } from "antd";
 import { useTranslation } from "react-i18next";
-import { NavLink, Chat, FooterContainer } from "./styles";
+import { NavLink, Chat } from "./styles";
 import NameLogo from "../NameLogo";
 import styled from "styled-components";
 import {
@@ -12,8 +12,19 @@ import {
 const { Footer } = Layout;
 
 const FooterSection = styled(Footer)`
+  display: flex;
   gap: 2rem;
   padding: 2.5rem;
+  text-align: center;
+`;
+
+const IconContainer = styled("div")`
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  transition: all 0.3s ease-in-out;
+  font-size: 1.8rem;
 `;
 
 const CustomFooter = () => {
@@ -21,39 +32,36 @@ const CustomFooter = () => {
 
   return (
     <FooterSection>
-      <Row justify="center">
-        <Col>
-          <NavLink to="mailto:knnthgms@gmail.com">
-            <Chat>{t(`Email me`)}</Chat>
-          </NavLink>
-          <FooterContainer>
-            <a
-              href="https://github.com/knnthgms"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <GithubOutlined />
-            </a>
-            <a
-              href="https://twitter.com/knnthgms"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <TwitterOutlined />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/knnthgms"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <LinkedinOutlined />
-            </a>
-          </FooterContainer>
-          <NavLink to="/">
-            <NameLogo />
-          </NavLink>
-        </Col>
-      </Row>
+      <NavLink to="/">
+        <NameLogo />
+      </NavLink>
+
+      <IconContainer>
+        <a
+          href="https://github.com/knnthgms"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GithubOutlined />
+        </a>
+        <a
+          href="https://twitter.com/knnthgms"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <TwitterOutlined />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/knnthgms"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedinOutlined />
+        </a>
+      </IconContainer>
+      <a href="mailto:knnthgms@gmail.com">
+        <Chat>{t(`Email me`)}</Chat>
+      </a>
     </FooterSection>
   );
 };
